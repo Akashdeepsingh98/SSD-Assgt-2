ac1f7ee95b010fff7db767a27c836755353e2822
Akashdeep <akashdeepsingh98@gmail.com>
2020-09-30 19:34:39 +0530

    collapsible

---
+++ jsreading.js
@@ -0,0 +1,18 @@
+var listitems = document.getElementsByTagName("button");
+var i;
+for (i = 0; i < listitems.length; i++) {
+    listitems[i].addEventListener("click", function () {
+        var collapsible = this.nextElementSibling;
+        if (collapsible.style.display === "block") {
+            collapsible.style.display = "none";
+        }
+        else {
+            collapsible.style.display = "block";
+        }
+    });
+}
+
+/*var ps=document.getElementsByTagName("p");
+for(i=0;i<ps.length;i++){
+    ps.style.display="block";
+}*/
